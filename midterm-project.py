ac9c7a3d8533b42dda5b8456425ebf18a03d9479
aadmys <112637334+aadmys@users.noreply.github.com>
2022-11-06 19:15:07 -0500

    Add files via upload

---
+++ midterm-project.py
@@ -0,0 +1,9 @@
+numbers = open('numbers.txt', 'r')
+listitems = numbers.read().splitlines()
+nums = {}
+for number in listitems:
+    if number in nums:
+        nums[number] += 1 
+    else:
+        nums[number] = 1 
+print(nums)
